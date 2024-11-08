fed9dcebdbddc3b741a42bf14a025f3c60fcf4a4
cryptic0053 <anirbanargha0053@gmail.com>
2024-11-09 01:11:34 +0600

    1

---
+++ server/README.md
@@ -0,0 +1,6 @@
+# E-Commerce MERN stack project
+## Course plan
+## Environment setup
+## Express server setup
+## HTTP request & response
+## nodemon and morgan package
---
+++ server/src/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require('morgan');
+const app = express();
+
+
+app.use(morgan('dev'));
+
+app.get('/test',(req,res)=>{
+    res.status(200).send(
+        {
+            message:'API testing is working fine',
+        }
+    );
+});
+
+app.listen(3001, ()=>{
+console.log(`server is running at http://localhost:3001`)
+});
